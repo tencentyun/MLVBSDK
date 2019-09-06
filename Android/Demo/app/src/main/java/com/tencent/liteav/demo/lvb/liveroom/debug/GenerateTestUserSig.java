d5e9dc9ce129f3f3add0a50af29ec8fe1ef07fab
carolsuo <carolsuo@tencent.com>
2019-09-06 21:16:23 +0800

    更新Andoid SDK到6.6.7551

--- Android/Demo/app/src/main/java/com/tencent/liteav/demo/lvb/liveroom/debug/GenerateTestUserSig.java
+++ Android/Demo/app/src/main/java/com/tencent/liteav/demo/lvb/liveroom/debug/GenerateTestUserSig.java
@@ -41,7 +41,7 @@ public class GenerateTestUserSig {
      * 进入腾讯云实时音视频[控制台](https://console.cloud.tencent.com/rav ) 创建应用，即可看到 SDKAppId，
      * 它是腾讯云用于区分客户的唯一标识。
      */
-    public static final int SDKAPPID = 1400047134;
+    public static final int SDKAPPID = 设置成您自己账号下的SDKAppId;
 
 
     /**
@@ -63,7 +63,7 @@ public class GenerateTestUserSig {
      * 注意：该方案仅适用于调试Demo，正式上线前请将 UserSig 计算代码和密钥迁移到您的后台服务器上，以避免加密密钥泄露导致的流量盗用。
      * 文档：https://cloud.tencent.com/document/product/647/17275#Server
      */
-    public static final String SECRETKEY = "";
+    public static final String SECRETKEY = 设置成您自己账号的加密密钥;
 
     /**
      * 计算 UserSig 签名
