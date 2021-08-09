3baa72e02c7b5752564ad02dc2d7c5a4505d8e0a
abyyxwang <abyyxwang@tencent.com>
2021-08-09 21:06:03 +0800

    iOS 更新9.0

---
+++ iOS/MLVB-API-Example-OC/Advanced/CustomVideoCapture/Helper/CustomCameraHelper.h
@@ -0,0 +1,35 @@
+//
+//  CustomCameraHelper.h
+//  MLVB-API-Example-OC
+//
+//  Created by abyyxwang on 2021/4/22.
+//
+
+#import <Foundation/Foundation.h>
+#import <AVFoundation/AVFoundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol CustomCameraHelperSampleBufferDelegate <NSObject>
+
+- (void)onVideoSampleBuffer:(CMSampleBufferRef)videoBuffer;
+
+@end
+
+@interface CustomCameraHelper : NSObject
+
+@property (weak, nonatomic)id<CustomCameraHelperSampleBufferDelegate>delegate;
+// set this property before start capture;
+@property (assign, nonatomic)UIInterfaceOrientation windowOrientation;
+
+- (void)checkPermission;
+
+- (void)createSession;
+
+- (void)startCameraCapture;
+
+- (void)stopCameraCapture;
+
+@end
+
+NS_ASSUME_NONNULL_END
